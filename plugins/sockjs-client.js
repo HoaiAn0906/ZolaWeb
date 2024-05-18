45bf431658f95125c384d49abeddfad116e7e6bd
An Phan Hoài <phoaianvlogs@gmail.com>
2024-05-19 00:36:15 +0700

    asdasd

--- plugins/sockjs-client.js
+++ plugins/sockjs-client.js
@@ -3,7 +3,7 @@ import Stomp from 'stompjs'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const sock = new SockJS('http://16.162.105.192/ws')
+  const sock = new SockJS('https://learning-website-hoaian.io.vn/ws')
   const stompClient = Stomp.over(sock)
   nuxtApp.provide('stompClient', stompClient)
 })
